23d92a7e5918cdc983149458787a4f769976bb6b
Roel Tombozafy <rtombozafy96@gmail.com>
2026-02-10 19:33:06 +0300

    Initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getEmoji(): string {
+    const emojis = this.getEmojis();
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    return emojis[randomIndex];
+  }
+
+  getEmojis() {
+    return [`😆`, `😍`, `🥶`, `👋`, `💪`, `👀`, `👣`, `👧`, `🌂`, `🚴🏼‍♂️`, `🧑🏽‍🎄`];
+  }
+}
