67a5ec1210efff4cdf981a3b76978717480dd3b0
Roel Tombozafy <rtombozafy96@gmail.com>
2026-02-11 10:52:06 +0300

    Added interceptor

---
+++ src/common/browser/browser.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BrowserInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const userAgent = request.headers['user-agent'];
+    const browserClient = userAgent.split(' ')[0] || 'Unknown';
+    request.headers.browser = browserClient;
+    console.log(
+      `Interceptor: manipulated request with new browser header: ${request.headers.browser}`,
+    );
+    return next.handle();
+  }
+}
