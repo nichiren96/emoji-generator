845a5f166ff218489b3f5e859c99e8ee36fe878b
Roel Tombozafy <rtombozafy96@gmail.com>
2026-02-11 19:07:42 +0300

    Interceptor after handler

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getEmoji(index?: number): string {
+    console.log('Route handler: generating emoji');
     const emojis = this.getEmojis();
     const emojiIndex = index || Math.floor(Math.random() * emojis.length);
     return emojis[emojiIndex];
---
+++ src/common/transform-response/transform-response.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class TransformResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        console.log('Interceptor: transforming response');
+        return {
+          success: true,
+          data,
+        };
+      }),
+    );
+  }
+}
