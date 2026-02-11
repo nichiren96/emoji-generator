ebfb6822400708ca265ac5856a1091cbcc3e2675
Roel Tombozafy <rtombozafy96@gmail.com>
2026-02-11 18:42:05 +0300

    Handlers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EmojiValidationPipe } from './common/emoji-validation/emoji-validation.pipe';
+import { type Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Query('index', EmojiValidationPipe) index?: number): string {
-    return this.appService.getEmoji(index);
+  getHello(
+    @Req() request: Request,
+    @Query('index', EmojiValidationPipe) index?: number,
+  ) {
+    return {
+      emoji: this.appService.getEmoji(index),
+      browser: request.headers.browser,
+    };
   }
 }
